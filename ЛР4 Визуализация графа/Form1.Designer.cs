902d465b1c69961617b8ce0495db4f47ad4e1d71
DmitryTheCreator <dmitriyzamorev@gmail.com>
2020-12-09 16:52:19 +0500

    Initial commit

---
+++ ЛР4 Визуализация графа/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ЛР4_Визуализация_графа
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.tbStart = new System.Windows.Forms.TextBox();
+            this.Matrix = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.Numb_TextBox = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Matrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Location = new System.Drawing.Point(12, 46);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(572, 559);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            // 
+            // tbStart
+            // 
+            this.tbStart.Location = new System.Drawing.Point(806, 159);
+            this.tbStart.Name = "tbStart";
+            this.tbStart.Size = new System.Drawing.Size(307, 26);
+            this.tbStart.TabIndex = 1;
+            // 
+            // Matrix
+            // 
+            this.Matrix.AllowUserToDeleteRows = false;
+            this.Matrix.AllowUserToResizeColumns = false;
+            this.Matrix.AllowUserToResizeRows = false;
+            this.Matrix.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Matrix.BackgroundColor = System.Drawing.Color.White;
+            this.Matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Matrix.Location = new System.Drawing.Point(590, 211);
+            this.Matrix.Name = "Matrix";
+            this.Matrix.ReadOnly = true;
+            this.Matrix.RowHeadersWidth = 51;
+            this.Matrix.RowTemplate.Height = 28;
+            this.Matrix.Size = new System.Drawing.Size(576, 394);
+            this.Matrix.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.ForeColor = System.Drawing.Color.Black;
+            this.btnSearch.Location = new System.Drawing.Point(850, 78);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(148, 34);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "поиск в глубину";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Numb_TextBox
+            // 
+            this.Numb_TextBox.Location = new System.Drawing.Point(884, 46);
+            this.Numb_TextBox.Name = "Numb_TextBox";
+            this.Numb_TextBox.Size = new System.Drawing.Size(74, 26);
+            this.Numb_TextBox.TabIndex = 3;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInput.ForeColor = System.Drawing.Color.White;
+            this.lblInput.Location = new System.Drawing.Point(757, 9);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(311, 25);
+            this.lblInput.TabIndex = 4;
+            this.lblInput.Text = "Введите начальную вершину:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(661, 158);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(127, 25);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Результат:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(123, 31);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(1178, 617);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.Numb_TextBox);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.Matrix);
+            this.Controls.Add(this.tbStart);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Matrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.TextBox tbStart;
+        private System.Windows.Forms.DataGridView Matrix;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox Numb_TextBox;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ЛР4 Визуализация графа/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ЛР4_Визуализация_графа
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();        
+        }
+        // Класс круг, его координаты и радиус
+        class Circle 
+        {
+            private int x, y;
+            private int rad = 15;
+            public int X { get { return x; } set { x = value; } }
+            public int Y { get { return y; } set { y = value; } }
+            public int Rad { get { return rad; } }
+        }
+
+        LinkedList<Circle> circles = new LinkedList<Circle>();
+        // Переменная, необходимая для обработки различных ситуаций при нажатии клавишей мыши
+        int versh = -1;
+        // Проверка для заполнения первых двух столбцов
+        bool check = false;
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            Circle circle = new Circle();
+            switch (e.Button)
+            {
+                // Нажата ПКМ
+                case MouseButtons.Right:
+                    {
+                        // Рисуется круг по координатам и число внутри круша
+                        Pen pen_default = new Pen(Color.Red, 2);
+                        
+                        circle.X = e.X - circle.Rad;
+                        circle.Y = e.Y - circle.Rad;
+                        Canvas.CreateGraphics().DrawEllipse(pen_default, circle.X, circle.Y,
+                            circle.Rad * 2, circle.Rad * 2);
+                        String drawString = (circles.Count + 1).ToString();
+                        int fontSize = 14;
+                        if (circles.Count + 1 >= 10)
+                            fontSize = 11;
+
+                        Font font = new Font("Arial", fontSize);
+                        SolidBrush brush = new SolidBrush(Color.Black);
+                        PointF point = new PointF(circle.X + circle.Rad / 2, circle.Y + circle.Rad / 2);
+                        Canvas.CreateGraphics().DrawString(drawString, font, brush, point);
+                        circles.AddLast(circle);
+
+                        // Создание матрицы
+                        if (check == false)
+                        {
+                            var column1 = new DataGridViewColumn();
+                            column1.HeaderText = "0";
+                            column1.ReadOnly = true; 
+                            column1.Name = "0"; 
+                            column1.CellTemplate = new DataGridViewTextBoxCell();
+                            column1.DefaultCellStyle.BackColor = Color.White;
+                            column1.DefaultCellStyle.ForeColor = Color.Black;
+                            Matrix.Columns.Add(column1);
+                            Matrix.Columns.Add("1", "1");
+                            Matrix.Rows.Add();
+                            Matrix[0, 0].Value = 1;
+                            Matrix[1, 0].Value = 0;
+                            check = true;
+                        }
+                        else
+                        {
+                            for (int i = circles.Count; i < circles.Count + 2; ++i)
+                            {
+                                Matrix.Columns.Add(i.ToString(), i.ToString());
+                                Matrix.Rows.Add();
+                            }
+                            for (int i = 0; i < circles.Count; ++i)
+                                Matrix[0, i].Value = i + 1;
+                            Matrix[0, circles.Count - 1].Value = circles.Count;
+                            Matrix[circles.Count, circles.Count - 1].Value = 0;
+                        }
+                        break;
+                    }
+                     // Нажата ПКМ                    
+                case MouseButtons.Left:
+                    {
+                        // Если точка находится внутри круга
+                        Pen pen_selected = new Pen(Color.DeepPink, 2);
+                        // Если не запомнили ни одну из вершин
+                        if (versh == -1)
+                        {
+                            int count = -1;
+                            // Идет проверка на нахождение точки на одной из вершин
+                            foreach (Circle c in circles) 
+                            {
+                                count++;
+                                if (e.X - c.Rad <= c.X + c.Rad &&
+                                    e.X - c.Rad >= c.X - c.Rad &&
+                                    e.Y - c.Rad <= c.Y + c.Rad &&
+                                    e.Y - c.Rad >= c.Y - c.Rad)
+                                {
+                                    // Выделение точки особым цветом
+                                    versh = count;
+                                    Canvas.CreateGraphics().DrawEllipse(pen_selected, c.X, c.Y,
+                                    c.Rad * 2, c.Rad * 2);
+                                    break;
+                                }
+                            }
+                        }
+                        // Если по одной из вершин уже раннее щелкнули
+                        else
+                        {
+                            int toversh = -1;
+                            int count = -1;
+                            // Идет проверка на нахождение точки на одной из вершин
+                            foreach (Circle c in circles) 
+                            {
+                                count++;
+                                if (e.X - c.Rad <= c.X + c.Rad &&
+                                    e.X - c.Rad >= c.X - c.Rad &&
+                                    e.Y - c.Rad <= c.Y + c.Rad &&
+                                    e.Y - c.Rad >= c.Y - c.Rad)
+                                {
+                                    // Выделение точки особым цветом
+                                    toversh = count;
+                                    Canvas.CreateGraphics().DrawEllipse(pen_selected, c.X, c.Y,
+                                    c.Rad * 2, c.Rad * 2);
+                                    break;
+                                }
+
+                            }
+                            // Если выделились две вершины
+                            if ((toversh != -1) && (versh != toversh))
+                            {
+                                int it = -1;
+                                Point p1 = new Point(0, 0);
+                                Point p2 = new Point(0, 0);
+                                // Запоминаем координаты этих вершин
+                                foreach (Circle c in circles)
+                                {
+                                    it++;
+                                    if (it == versh)
+                                        p1 = new Point(c.X + c.Rad, c.Y + c.Rad);
+                                    if (it == toversh)
+                                        p2 = new Point(c.X + c.Rad, c.Y + c.Rad);                                 
+                                }
+                                // Рисуем линию
+                                Canvas.CreateGraphics().DrawLine(pen_selected, p1, p2);
+                                // Отмечаем на матрице
+                                Matrix[versh + 1, toversh].Value = 1;
+                                Matrix[toversh + 1, versh].Value = 1;
+                                versh = -1;                              
+                            }
+                        }
+                        break;
+                    }                  
+            } 
+            tbStart.Text = versh.ToString();
+        }
+        // Поиск в глубину
+        public void DFS(int start)
+        {
+            List<int> list = new List<int>(); 
+            // Для поиска в глубину используется стэк,
+            // а для поиска в ширину - очередь
+            Stack<int> s = new Stack<int>(); 
+            list.Add(start);
+            // Все не пустые значения ячеек таблицы засовываем в стек
+            for (int i = circles.Count; i >= 1; i--)
+            {
+                if (Matrix.Rows[start - 1].Cells[i].Value == null)
+                    continue;
+                s.Push(i);
+            }
+            rep:
+            // Пока стек не пуст вынимаем из него число.
+            // Засовываем его в лист
+            while (s.Count != 0)
+            {
+                int v = s.Peek();
+                s.Pop();
+                foreach (int i in list)
+                {
+                    if (i == v)
+                        goto rep;
+                }
+                list.Add(v);
+                // Если содержимое ячейки не пусто - засовываем в стек
+                for (int i = circles.Count; i >= 1; i--)
+                {
+                    if (Matrix.Rows[v - 1].Cells[i].Value == null)
+                        continue;
+                    s.Push(i);
+                }
+            }
+            tbStart.Text = "";
+            // Выводим все элементы списка в TextBox
+            foreach (int i in list)
+                 tbStart.Text += i.ToString() + " ";
+        }
+        // Запускаем поиск в глубину
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int start = Convert.ToInt32(Numb_TextBox.Text);
+            DFS(start);
+        }
+        // Очищаем экран, список кругов и таблицу
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Matrix.Columns.Clear();
+            Matrix.Rows.Clear();
+            Canvas.Refresh();
+            circles.Clear();
+            Matrix.Columns.Add(0.ToString(), 0.ToString());
+        }
+    }
+}
